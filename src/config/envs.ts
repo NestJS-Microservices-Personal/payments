293114df66a9c7af96c4c21616a23a037c61411f
Jander Gomez <jandergb.30gmail.com>
2024-10-30 21:55:32 +0100

    feat: add hybrid server: TCP & NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,7 @@ interface EnvVars {
   STRIPE_WEBHOOK_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
+  NATS_SERVERS:string[];
 }
 
 const envSchema= Joi.object({
@@ -14,7 +15,8 @@ const envSchema= Joi.object({
   STRIPE_SECRET_KEY: Joi.string().required(),
   STRIPE_WEBHOOK_SECRET: Joi.string().required(),
   STRIPE_SUCCESS_URL: Joi.string().required(),
-  STRIPE_CANCEL_URL: Joi.string().required()
+  STRIPE_CANCEL_URL: Joi.string().required(),
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 })
 .unknown(true)
 
@@ -33,5 +35,6 @@ export const envs ={
   stripeWebhookSecret: envVars.STRIPE_WEBHOOK_SECRET,
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  natsServers: envVars.NATS_SERVERS,
 }
 
